33579081db0e1e89f3d687b21d201789a937b785
Aliaksandr Malakhau <altius200@gmail.com>
2023-11-27 22:15:25 +0200

    simple playwright test

---
+++ tests/test_r.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_pop():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://onliner.by")
+    driver.find_element(By.XPATH, "//div[@class='auth-bar__item auth-bar__item--text']").click()
+    driver.find_element(By.XPATH, "//input[@placeholder='Ник или e-mail']").send_keys('Name')
+    driver.find_element(By.XPATH, "//input[@placeholder='Пароль']").send_keys('name')
+    driver.find_element(By.XPATH, "//button[contains(text(),'Войти')]").click()
+    time.sleep(4)
+
+    test = "//div[@class='auth-form__title auth-form__title_base auth-form__title_condensed-other']"
+    assert test == "Помогите нам улучшить безопасность"
