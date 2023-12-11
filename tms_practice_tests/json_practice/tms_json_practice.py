220ff8434eb5620260dcdd3e730820ccd4f383d8
Malahov-Alexandr <67895548+Malahov-Alexandr@users.noreply.github.com>
2023-12-11 14:19:34 +0200

    Update tms_json_practice.py

--- tms_practice_tests/json_practice/tms_json_practice.py
+++ tms_practice_tests/json_practice/tms_json_practice.py
@@ -12,7 +12,7 @@ with open('HW_Files.txt', 'r', encoding='utf-8') as f:
     for v in value.split():
         value_for_dict = v.split(',')
         new_dict[value_for_dict[0]] = value_for_dict[1]
-    print(new_dict)
+
 
 with open('test_file.json', 'w',encoding='utf-8') as new_file:
     json.dump(new_dict, new_file,ensure_ascii=False,indent=4,sort_keys=True)
